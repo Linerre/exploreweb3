4571f8735921cf45e355583d45ddff138bff0256
Linerre <leon.errelin@outlook.com>
2025-11-10 17:50:31 +0800

    Add proxy to ensure connection success

--- ex1/task1/web3js/query.js
+++ ex1/task1/web3js/query.js
@@ -1,10 +1,20 @@
 import { Web3 } from 'web3';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 
 const ENDPOINT = 'https://mainnet.infura.io/v3/d0b4d2a52c834debbe7fb2481e5c1ac7';
 const TX_HASH = '0x2ad2bb00718ab0ed8310dacff9c029ea5d41e038d96c9f52561a1e7948759e99';
 
 async function getTxInfo(txHash) {
-    const web3 = new Web3(ENDPOINT);
+    // I'm living behind GFW so I need the proxy. You probably don't.
+    const proxyUrl = process.env.https_proxy || process.env.HTTPS_PROXY;
+    const proxyAgent = new HttpsProxyAgent(proxyUrl);
+
+    const web3 = new Web3(new Web3.providers.HttpProvider(
+        ENDPOINT,
+        {
+            providerOptions: { agent: proxyAgent }
+        }
+    ));
 
     try {
         const txInfo = await web3.eth.getTransaction(txHash);
@@ -16,7 +26,7 @@ async function getTxInfo(txHash) {
         const {to, input, gasPrice} = txInfo;
         console.log('========= Query using Web3.js =========')
         console.log('To address:', to);
-        console.log('Input call data (raw):', input);
+        console.log('Input call data (encoded):', input);
         console.log('Gas Price:', gasPrice);
     } catch (e) {
         console.error('Error getting tx info:', e.message);
